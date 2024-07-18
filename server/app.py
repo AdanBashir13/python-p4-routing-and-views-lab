02838c025d5a66154e74f4daa19e082aecb10b8b
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-07-18 19:10:19 +0300

    done with lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:name>")
+def print_string(name):
+    print(name)
+    return name
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
+
+@app.route("/math/<int:num1>/<string:operation>/<int:num2>")
+def math(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "%":
+        result = num1 % num2
+    elif operation == "div":
+        if num2 != 0:
+            result = num1/num2
+        else:
+            return "Error: not divisible by zero"
+
+    else:
+        return "Invalid operation"
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
